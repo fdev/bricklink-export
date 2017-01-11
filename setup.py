2e687acb2c3eedf7319bb5353ab24e6df2603b08
Folkert de Vries <f.devries@payt.nl>
2017-01-11 17:16:30 +0100

    Make compatible with new BrickLink

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except:
 
 setup(
 	name = "bricklink-export",
-	version = "1.0",
+	version = "1.1",
 	url = 'http://github.com/fdev/bricklink-export/',
 	license = 'GPL2',
 	description = description,
--- src/bricklink_export/__init__.py
+++ src/bricklink_export/__init__.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 
 import itertools
 import argparse
+import json
 import sys
 import re
 import os
@@ -42,7 +43,7 @@ def main():
 	
 	# Command arguments
 	parser = argparse.ArgumentParser(description='Export a BrickLink wanted list.')
-	parser.add_argument('--version', action='version', version='bricklink-export 1.0')
+	parser.add_argument('--version', action='version', version='bricklink-export 1.1')
 	parser.add_argument('-v', '--verbose', dest='verbose', action='store_true', default=False, help='be verbose')
 	parser.add_argument('-u', '--username', dest='username', help='username on BrickLink')
 	parser.add_argument('-p', '--password', dest='password', help='password on BrickLink (omit for prompt)')
@@ -54,7 +55,7 @@ def main():
 	
 	# Requests session
 	session = requests.Session()
-	session.headers.update({'User-Agent': 'bricklink-export 1.0 (http://github.com/fdev/bricklink-export)'})
+	session.headers.update({'User-Agent': 'bricklink-export 1.1 (http://github.com/fdev/bricklink-export)'})
 	
 	# List and export require authentication
 	if args.list or args.export is not None:
@@ -100,34 +101,38 @@ def main():
 		# Authenticate
 		verbose('Authenticating.')
 		payload = {
-			'a': 'a',
-			'logFrmFlag': 'Y',
-			'frmUsername': username, 
-			'frmPassword': password, 
+			'pageId': 'LOGIN',
+			'userid': username,
+			'password': password,
 		}
-		r = session.post('https://www.bricklink.com/login.asp', data=payload, allow_redirects=False)
+		r = session.post('https://www.bricklink.com/ajax/renovate/login.ajax', data=payload, allow_redirects=False)
 
 		if not r:
 			sys.exit('Could not log in to BrickLink.')
 
-		if r.status_code != 302 or r.headers['location'] != '/pageRedirect.asp?p=my.asp':
+		try:
+			data = json.loads(r.text)
+		except:
+			sys.exit('Invalid JSON in authentication response.')
+
+
+		if data.get('returnCode') != 0:
 			sys.exit('Invalid username or password specified.')
 
 		verbose('Authenticated as %s.' % username)
 
-	# Fetch list of colors for color list and export
-	if args.colors or args.export is not None:
-		colors = []
-		
+	# Color list
+	if args.colors:
 		verbose('Retrieving color guide.')
-		r = session.get('http://www.bricklink.com/catalogColors.asp')
+		r = session.get('https://www.bricklink.com/catalogColors.asp')
 		if not r:
 			sys.exit('Could not retrieve color guide.')
 
 		verbose('Parsing.')
 		html = pq(r.text)
 		tables = html('table tr')
-	
+
+		colors = []
 		for table in map(pq, tables):
 			if len(table('td')) == 9 and strip(table('td:nth-child(9)').text()) != 'Color Timeline':
 				id = strip(table('td:nth-child(1)').text())
@@ -140,9 +145,7 @@ def main():
 					'id': id,
 					'name': name,
 				})
-	
-	# Color list
-	if args.colors:
+
 		print('ID\tName')
 		for color in colors:
 			print('%s\t%s' % (color['id'], color['name']))
@@ -151,141 +154,79 @@ def main():
 	# List of wanted lists
 	if args.list:
 		verbose('Retrieving list of wanted lists.')
-		r = session.get('http://www.bricklink.com/wantedView.asp')
+		r = session.get('https://www.bricklink.com/v2/wanted/list.page')
 		if not r:
 			sys.exit('Could not retrieve wanted lists.')
 
 		verbose('Parsing.')
-		html = pq(r.text)
-		table = html('form[action="wantedView.asp?a=md"]').closest('table')
-		header = table('tr:eq(0)')
-		rows = table('tr:gt(0)')
-	
-		# Assert expected header
-		for col, label in enumerate(('ID', 'Name', 'Items')):
-			td = strip(header('td:eq(%d)' % col).text())
-			if td != label:
-				sys.exit('Unexpected wanted list table format.')
-	
+
+		match = re.search(r'var wlJson = (\{.+?\});\r?\n', r.text, re.MULTILINE)
+		if not match:
+			sys.exit('Unexpected wanted list page format.')
+
+		try:
+			data = json.loads(match.group(1))
+		except:
+			sys.exit('Invalid JSON found in wanted list page.')
+
+		# Assert expected data
+		if not isinstance(data, dict) or not isinstance(data.get('wantedLists'), list):
+			sys.exit('Unexpected JSON content in wanted list page.')
+
 		# Print list
 		print('ID\tItems\tName')
-		for row in map(pq, rows):
-			id = row('td:eq(0)').text().strip()
-			if row('td:eq(1) input'):
-				name = row('td:eq(1) input:first').attr('value')
-			else:
-				# First row is static
-				name = row('td:eq(1)').text().strip()
-			items = row('td:eq(2)').text().strip() or '0'
-
-			print('%s\t%s\t%s' % (id, items, name))
+		for row in data['wantedLists']:
+			print('%s\t%s\t%s' % (row['id'], row['num'], row['name']))
 	
 		sys.exit()
 
 	# Export
 	if args.export is not None:
 		items = []
-	
-		# Sort colors by name length (longest first)
-		itemcolors = sorted(colors, key=lambda color: len(color['name']), reverse=True)
-		
-		# The wanted list is split per item type
-		itemtypes = (
-			('S', 'sets'),
-			('P', 'parts'),
-			('M', 'minifigs'),
-			('B', 'books'),
-			('G', 'gear'),
-			('C', 'catalogs'),
-			('I', 'instructions'),
-			('O', 'original boxes'),
-			('U', 'unsorted'),
-		)
 
-		for itemtype, name in itemtypes:
-			for page in itertools.count(1):
-				verbose('Retrieving page %d of item type %s...' % (page, name))
-				payload = {
-					'pg': page,
-					'catType': itemtype,
-					'wantedMoreID': args.export,
-				}
-				r = session.get('http://www.bricklink.com/wantedDetail.asp', params=payload)
-				if not r:
-					sys.exit('Could not retrieve page %d of part list.' % page)
+		for page in itertools.count(1):
+			verbose('Retrieving page %d...' % page)
 
-				verbose('Parsing.')
-				html = pq(r.text)
-				form = html('form[name="wantedForm"]')
-				
-				if not form:
-					# No items on this page
-					break
+			r = session.get('https://www.bricklink.com/v2/wanted/search.page?type=A&wantedMoreID=%d&sort=1&pageSize=100&page=%d' % (args.export, page))
+			if not r:
+				sys.exit('Could not retrieve page %d of part list.' % page)
 
-				table = form.closest('table')
-				header = table('tr:eq(0)')
-				rows = table.find('tr:gt(0)')
-	
-				# Assert expected header
-				for col, label in enumerate(('Image', 'Notify', 'Condition', 'Min Qty', 'Max Price', 'My Remarks')):
-					td = strip(header('td:eq(%d)' % col).text())
-					if td != label:
-						sys.exit('Unexpected wanted list table format.')
-	
-				if len(rows) % 4 != 0:
-					sys.exit('Unexpected wanted list table length.')
+			verbose('Parsing.')
 
-				for row1, row2, row3, row4 in zip(*([iter(map(pq, rows))] * 4)):
-					# Part number
-					link = row1('td:eq(2) a[href]').attr('href')
-					match = re.search('(catalogItem\.asp|catalogitem\.page)\?[SPMBGCIOU]=([0-9a-zA-Z\-]+)', link)
-					if not match:
-						sys.exit('Unexpected wanted list item link format.')
-					item = match.group(1)
-		
-					# Part name
-					name = row1('td:eq(2)').text().strip()
-		
-					# Notify
-					notify = 'Y' if row2("td:eq(0) input[type='CHECKBOX']").attr('checked') else 'N'
-				
-					# Condition
-					condition = row2("td:eq(1) input[type='hidden']").attr('value')
-		
-					# Minimum quantity
-					minquantity = row2("td:eq(2) input[type='hidden']").attr('value')
-		
-					# Maximum price
-					maxprice = row2("td:eq(3) input[type='hidden']").attr('value')
-		
-					# Remarks
-					remarks = row2("td:eq(6) input[type='TEXT']").attr('value')
+			match = re.search(r'var wlJson = (\{.+?\});\r?\n', r.text, re.MULTILINE)
+			if not match:
+				sys.exit('Unexpected wanted page format.')
 
-					# Color (only for parts)
-					color = None
-					if itemtype == 'P':
-						for itemcolor in itemcolors:
-							# Name starts with color name.
-							if name.startswith(itemcolor['name'] + ' '):
-								color = itemcolor['id']
-								break
-		
-					items.append({
-						'type': itemtype,
-						'id': item,
-						'color': color,
-						'name': encode(name),
-						'condition': condition,
-						'minquantity': minquantity,
-						'maxprice': maxprice,
-						'remarks': encode(remarks),
-						'notify': notify,
-					})
-				
-				if not html("a[href]:contains('Next')"):
-					break
-		
-		# See http://www.bricklink.com/help.asp?helpID=207
+			try:
+				data = json.loads(match.group(1))
+			except:
+				sys.exit('Invalid JSON found in wanted page.')
+
+			# Assert expected data
+			if not isinstance(data, dict) or not isinstance(data.get('wantedItems'), list):
+				sys.exit('Unexpected JSON content in wanted page.')
+
+			if not data['wantedItems']:
+				# No items on this page
+				break
+
+			for row in data['wantedItems']:
+				items.append({
+					'type': row['itemType'],
+					'id': row['itemNo'],
+					'color': row['colorID'],
+					'name': encode(row['itemName']),
+					'condition': row['wantedNew'],
+					'minquantity': row['wantedQty'],
+					'maxprice': row['wantedPrice'],
+					'remarks': encode(row['wantedRemark'] or ''),
+					'notify': row['wantedNotify'],
+				})
+
+			if len(items) == data.get('totalResults'):
+				break
+
+		# See https://www.bricklink.com/help.asp?helpID=207
 		print('<INVENTORY>')
 		for item in items:	
 			print('\t<ITEM>')
@@ -294,7 +235,7 @@ def main():
 			if item['color']:
 				print('\t\t<COLOR>%s</COLOR>' % item['color'])
 			print('\t\t<CONDITION>%s</CONDITION>' % item['condition'])
-			if item['maxprice']:
+			if item['maxprice'] > 0:
 				print('\t\t<MAXPRICE>%s</MAXPRICE>' % item['maxprice'])
 			if item['minquantity']:
 				print('\t\t<MINQTY>%s</MINQTY>' % item['minquantity'])
